f9024ee02a485573ded38becb26878c7729d21a9
“viveks-php” <vivek.sharma>
2026-03-18 01:30:52 +0530

    Fix gitignore - only keep dist

--- frontend/src/modules/dashboard/components/DashboardSection.jsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardSection({ children }) {
-  return (
-    <div className="container-fluid mt-4">
-      <div className="row g-4">{children}</div>
-    </div>
-  );
-}
--- frontend/src/modules/dashboard/dashboard.api.js
+++
@@ -1,6 +0,0 @@
-import api from "../../app/axios";
-
-export const fetchDashboardStats = async () => {
-    const response = await api.get("/dashboard/stats");
-    return response.data.data;
-};
